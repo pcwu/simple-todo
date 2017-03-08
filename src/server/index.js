363e4af8ee9f671a8fee0aa66464b641610660fd
Po-Chun Wu <sb1126@gmail.com>
2017-03-08 17:09:25 +0100

    edit server/index.js

--- src/server/index.js
+++ src/server/index.js
@@ -5,17 +5,19 @@ import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 
+import './db';
 import webpackConfig from '../../webpack.config';
 import Todos from './api/todos';
-import './db';
+import config from './config';
 
 const app = express();
+const { ENVIRONMENT, PORT } = config;
 
-if (process.env.NODE_ENV === 'development') {
+if (ENVIRONMENT === 'development') {
   const compiler = webpack(webpackConfig);
   app.use(webpackDevMiddleware(compiler, { publicPath: webpackConfig.output.publicPath }));
   app.use(webpackHotMiddleware(compiler));
-};
+}
 
 app.use(morgan(':status :method :url HTTP/:http-version :date[iso] :response-time ms'));
 app.use(bodyParser.json());
@@ -29,10 +31,10 @@ app.use((req, res, next) => {
   next(err);
 });
 
-app.listen(3000, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     console.error(err);
   } else {
-    console.info('==> 🚧   Server listening on port %s', 3000);
+    console.info('==> 🚧   Server listening on port %s', PORT);
   }
 });
